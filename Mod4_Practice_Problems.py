650e901038fd7dfecedd8e7c0d406c7e9405a3dd
Mason Hite <Caffeinatedmc@gmail.com>
2025-02-08 11:20:43 -0800

    Update Mod4_Practice_Problems.py

--- Mod4_Practice_Problems.py
+++ Mod4_Practice_Problems.py
@@ -10,7 +10,7 @@
 # 2. The headlights of a car should only automatically turn on when the daylight outside is below a certain threshold. If a sensor threshold is below the number 8, print "Headlights On"; otherwise, print "Headlights Off".
 
 daylight = float(input("Enter the amount of daylight: "))
-if daylight <= 8:
+if daylight < 8:
     print("Headlights On")
 else:
     print("Headlights Off")
